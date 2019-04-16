2e799db4820fe9637333080458e1229b331ba133
Kevin Falting <kevinfalting@gmail.com>
2019-04-16 10:06:29 +0900

    add readme and add /pi endpoint. location data stored in memory.

---
+++ README.md
@@ -0,0 +1,61 @@
+# Bus GPS API
+
+This is the server application that will be running as the middle person between the gps units on the the busses and the front end to display the bus locations.
+
+There is no data being persisted in a database, the application just stores the most recent location data for each route in memory.
+
+###The api endpoints are as follows:
+
+`/api` GET will return the following data:
+```typescript
+{
+  1: {
+    loc: Number,
+    err: String
+  },
+  2: {
+    loc: Number,
+    err: String
+  }
+}
+```
+
+`/pi` POST expects the following data:
+```typescript
+{
+  line: Number,
+  loc: Number
+}
+```
+### Keys
+Blue Line = 1
+Orange Line = 2
+
+Location number key:
+1 - MAIN GATE
+2 - TRANSIT
+3 - COMMISSARY
+4 - TRANSIT
+5 - BLDG 474 (SNCO DORMS)
+6 - TRANSIT
+7 - BLDG 383 (SFS DORMS)
+8 - TRANSIT
+9 - BLDG 657 (CE CUST SVC)
+10 - TRANSIT
+11 - PASSENGER TERMINAL
+12 - TRANSIT
+13 - DBIDS
+14 - TRANSIT
+15 - BLDG 936 (MPF/7 AF)
+16 - TRANSIT
+17 - BX
+18 - TRANSIT
+19 - IPE
+20 - TRANSIT
+21 - COMMAND WARRIOR ECP
+22 - TRANSIT
+23 - BLDG 1601 (GOLF CRSE)
+24 - TRANSIT
+25 - MUSTANG CENTER
+
+
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var blueLoc = 1;
 var orangeLoc = 1;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // redirect home to blue line
 app.get('/', (req, res) => res.redirect('/blue'));
@@ -22,24 +24,37 @@ app.get('/orange', (req, res) => {
 });
 
 // the endpoint for the client to update it's bus location
-// currently with test data
 app.get('/api', (req, res) => {
-  const testData = {
+  const locationData = {
     1: {
-      loc:1
+      loc: blueLoc
     },
     2: {
-      loc:14
+      loc: orangeLoc
     }
   };
   
-  res.json(testData);
+  res.json(locationData);
 });
 
 // the endpoint for the bus to post data to.
 // updates the global variables location for each one.
+/**
+ * Example incoming data
+ * 
+ * {line: 1,loc: 5}
+ */
 app.post('/pi', (req, res) => {
-  res.send(`this path receives data from the buses.`);
+
+  switch (parseInt(req.body.line)) {
+    case 1:
+      blueLoc = parseInt(req.body.loc);
+      break;
+    case 2:
+      orangeLoc = parseInt(req.body.loc);
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`bus GPS listening on port ${port}!`));
