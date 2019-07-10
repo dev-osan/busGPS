9d2bd7f4f06eb3d973ff584c3a9923e5c9b04bae
Kevin Falting <kevinfalting@gmail.com>
2019-07-10 15:41:23 +0900

    change route formatting

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
-export const routes = {
+export const routes_old = {
   'route': [
     ['Main Gate', 37.08054333333334, 127.04833],
     ['Commissary', 37.07875444444445, 127.04181222222223],
@@ -15,3 +15,21 @@ export const routes = {
     ['Mustang Center', 37.08081000000001, 127.02802666666668]
   ]
 };
+
+export const routes = {
+  "stops": [
+    { "name": "Main Gate",                      "lat": 37.08054333333334,  "lon": 127.04833 },
+    { "name": "Commissary",                     "lat": 37.07875444444445,  "lon": 127.04181222222223 },
+    { "name": "Bldg 474 (SNCO Dorms)",          "lat": 37.083195555555555, "lon": 127.04271555555556 },
+    { "name": "Bldg 383 (SFS Dorms)",           "lat": 37.08466777777778,  "lon": 127.04041555555558 },
+    { "name": "Bldg 667 (CE Customer Service)", "lat": 37.08597,           "lon": 127.03798000000003 },
+    { "name": "Passenger Terminal",             "lat": 37.08686222222222,  "lon": 127.03648333333336 },
+    { "name": "DBIDS",                          "lat": 37.08249333333334,  "lon": 127.03905222222222 },
+    { "name": "Bldg 936 (MPF/7AF)",             "lat": 37.08144333333333,  "lon": 127.03573111111112 },
+    { "name": "BX",                             "lat": 37.08386375,        "lon": 127.03390250000001 },
+    { "name": "IPE",                            "lat": 37.08489,           "lon": 127.03301999999998 },
+    { "name": "Commando Warrior ECP",           "lat": 37.08593625,        "lon": 127.02627625000001 },
+    { "name": "Bldg 1601 ECP (Golf Course)",    "lat": 37.082617,          "lon": 127.02308299999997 },
+    { "name": "Mustang Center",                 "lat": 37.08081000000001,  "lon": 127.02802666666668 }
+  ]
+}
