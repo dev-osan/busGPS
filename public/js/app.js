ff7240c0a442e0b91b673602c0804666c928f47b
Kevin Falting <kevinfalting@gmail.com>
2019-07-11 17:39:22 +0900

    update logic to accommodate new data format

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // This file will hold the logic for making a GET request to the API, parse the returned JSON, and update the screen. There is a blue line and a orange line, each running in opposite directions.
 
-// The fetch function should automatically update every 10 seconds.
+// The fetch function should automatically update every 5 seconds.
 
 // TODO: make a request for the current routes and build the page based on the route that is returned.
 
@@ -9,19 +9,20 @@
 /*
 
 JSON data structure
-1 - Blue Line
-2 - Orange Line
 loc - A number that represents the location of the Bus.
+intransit - A boolean describing if the bus is in transit FROM the location.
 err - A string with an error message. Empty string if none exist.
 
 {
-  1: {
+  "blue": {
     loc: Number,
-    err: String
+    intransit: Boolean,
+    err?: String
   },
-  2: {
+  "orange": {
     loc: Number,
-    err: String
+    intransit: Boolean,
+    err?: String
   }
 }
 
@@ -48,37 +49,40 @@ function updateBusLocations(data) {
   let blueLocation = data.blue.loc;
   let orangeLocation = data.orange.loc;
 
+  let blueInTransit = data.blue.intransit;
+  let orangeInTransit = data.orange.intransit;
+
   if (data.blue.err) {
     document.getElementById("blue-status").innerText = data.blue.err;
-  } else if (blueLocation % 2 != 0) {
+  } else if (!blueInTransit) {
     document.getElementById(`blue-${blueLocation}`).classList.add('active');
-    document.getElementById("blue-status").innerText = `Stopped at #${Math.round(blueLocation / 2)}`;
-  } else {
+    document.getElementById("blue-status").innerText = `Stopped at #${blueLocation}`;
+  } else if (blueInTransit) {
     document.getElementById(`blue-${blueLocation + 1}`).classList.add('active', 'blink');
-    document.getElementById("blue-status").innerText = `In transit to #${Math.round(blueLocation / 2) + 1}`;
+    document.getElementById("blue-status").innerText = `In transit to #${blueLocation + 1}`;
   }
 
   if (data.orange.err) {
     document.getElementById("orange-status").innerText = data.orange.err;
-  } else if (orangeLocation % 2 != 0) {
+  } else if (!orangeInTransit) {
     document.getElementById(`orange-${orangeLocation}`).classList.add('active');
-    document.getElementById("orange-status").innerText = `Stopped at #${Math.round(orangeLocation / 2)}`;
+    document.getElementById("orange-status").innerText = `Stopped at #${orangeLocation}`;
   } else {
     document.getElementById(`orange-${orangeLocation - 1}`).classList.add('active', 'blink');
-    document.getElementById("orange-status").innerText = `In transit to #${Math.round(orangeLocation / 2)}`;
+    document.getElementById("orange-status").innerText = `In transit to #${orangeLocation - 1}`;
   }
 
 }
 
 function deactivateAllBlueIcons() {
-  for (let i = 1; i <= 25; i += 2) {
+  for (let i = 1; i <= 13; i++) {
     document.getElementById(`blue-${i}`).classList.remove('active');
     document.getElementById(`blue-${i}`).classList.remove('blink');
   }
 }
 
 function deactivateAllOrangeIcons() {
-  for (let i = 1; i <= 25; i += 2) {
+  for (let i = 1; i <= 13; i++) {
     document.getElementById(`orange-${i}`).classList.remove('active');
     document.getElementById(`orange-${i}`).classList.remove('blink');
   }
