8f9a345e4df3d0403ed658894d795a845d325339
Kevin Falting <kevinfalting@gmail.com>
2019-07-12 09:56:46 +0900

    forgot to run tsc

--- routes.js
+++ routes.js
@@ -2,18 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.routes = {
     "stops": [
-        { "name": "Main Gate", "lat": 37.08054333333334, "lon": 127.04833 },
-        { "name": "Commissary", "lat": 37.07875444444445, "lon": 127.04181222222223 },
-        { "name": "Bldg 474 (SNCO Dorms)", "lat": 37.083195555555555, "lon": 127.04271555555556 },
-        { "name": "Bldg 383 (SFS Dorms)", "lat": 37.08466777777778, "lon": 127.04041555555558 },
-        { "name": "Bldg 667 (CE Customer Service)", "lat": 37.08597, "lon": 127.03798000000003 },
-        { "name": "Passenger Terminal", "lat": 37.08686222222222, "lon": 127.03648333333336 },
-        { "name": "DBIDS", "lat": 37.08249333333334, "lon": 127.03905222222222 },
-        { "name": "Bldg 936 (MPF/7AF)", "lat": 37.08144333333333, "lon": 127.03573111111112 },
-        { "name": "BX", "lat": 37.08386375, "lon": 127.03390250000001 },
-        { "name": "IPE", "lat": 37.08489, "lon": 127.03301999999998 },
-        { "name": "Commando Warrior ECP", "lat": 37.08593625, "lon": 127.02627625000001 },
-        { "name": "Bldg 1601 ECP (Golf Course)", "lat": 37.082617, "lon": 127.02308299999997 },
-        { "name": "Mustang Center", "lat": 37.08081000000001, "lon": 127.02802666666668 }
+        { "name": "Main Gate", "lat": 37.08054333333334, "lon": 127.04833, "blue_time": 0, "orange_time": 30 },
+        { "name": "Commissary", "lat": 37.07875444444445, "lon": 127.04181222222223, "blue_time": 2, "orange_time": 25 },
+        { "name": "Bldg 474 (SNCO Dorms)", "lat": 37.083195555555555, "lon": 127.04271555555556, "blue_time": 4, "orange_time": 23 },
+        { "name": "Bldg 383 (SFS Dorms)", "lat": 37.08466777777778, "lon": 127.04041555555558, "blue_time": 5, "orange_time": 21 },
+        { "name": "Bldg 667 (CE Customer Service)", "lat": 37.08597, "lon": 127.03798000000003, "blue_time": 6, "orange_time": 19 },
+        { "name": "Passenger Terminal", "lat": 37.08686222222222, "lon": 127.03648333333336, "blue_time": 7, "orange_time": 16 },
+        { "name": "DBIDS", "lat": 37.08249333333334, "lon": 127.03905222222222, "blue_time": 9, "orange_time": 15 },
+        { "name": "Bldg 936 (MPF/7AF)", "lat": 37.08144333333333, "lon": 127.03573111111112, "blue_time": 10, "orange_time": 12 },
+        { "name": "BX", "lat": 37.08386375, "lon": 127.03390250000001, "blue_time": 12, "orange_time": 10 },
+        { "name": "IPE", "lat": 37.08489, "lon": 127.03301999999998, "blue_time": 14, "orange_time": 8 },
+        { "name": "Commando Warrior ECP", "lat": 37.08593625, "lon": 127.02627625000001, "blue_time": 18, "orange_time": 5 },
+        { "name": "Bldg 1601 ECP (Golf Course)", "lat": 37.082617, "lon": 127.02308299999997, "blue_time": 20, "orange_time": 2 },
+        { "name": "Mustang Center", "lat": 37.08081000000001, "lon": 127.02802666666668, "blue_time": 30, "orange_time": 0 }
     ]
 };
